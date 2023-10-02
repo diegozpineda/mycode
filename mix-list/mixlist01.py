55d90977f61865e78f0a397c47fb244531dbffb4
diegozpineda <dzpineda86@gmail.com>
2023-10-02 18:55:53 +0000

    Lists (Chapter 60)

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+#
+
+#List with ip, port, portstate
+my_list = ["192.168.0.5", 5060, "UP" ]
+
+# print statements of each element in list
+print("The first item in the list (IP): " + my_list[0])
+
+print("The second item in the list (port): " + str(my_list[1]) )
+
+print("The last item in the list (state): " + my_list[2] )
+
+# challenge
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print(iplist[3])
