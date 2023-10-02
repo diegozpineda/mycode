eb737bf526a782d233d6ae223433a04dcc7cd036
diegozpineda <dzpineda86@gmail.com>
2023-10-02 15:22:37 +0000

    Challenge 54

---
+++ challenge-54.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+
+def record_info():
+    user_name = input("What is your name?: ")
+    week_day = input("what is the current day of the week?: ")
+
+    print(f'Hello, {user_name}! Happy {week_day}!')
+
+if __name__ == "__main__":
+   record_info() 
