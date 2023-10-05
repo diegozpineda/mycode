c170699c1161f63c84e76f65241233687677d2c4
diegozpineda <dzpineda86@gmail.com>
2023-10-05 20:31:35 +0000

    Chapter 106 - OOP and intro to class inheritence

---
+++ oop2/dice_test2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from cheatdice import Player
+from cheatdice import Cheat_Swapper
+from cheatdice import Cheat_Loaded_Dice
+
+def main():
+    
+    #create two cheater objects
+    cheater1 = Cheat_Swapper()
+    cheater2 = Cheat_Loaded_Dice()
+
+    
