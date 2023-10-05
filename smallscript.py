9d8bf5e64470f3e98de3808a488bb2eaa9be16ce
diegozpineda <dzpineda86@gmail.com>
2023-10-05 16:02:28 +0000

    Chapter 95 - intro to pylint

---
+++ smallscript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+'''
+Author: RZFeeser
+This program prints stuff to the screen
+'''
+def main():
+    '''
+    our main program
+    '''
+    mynote = "print this string"
+    print(mynote)
+
+main()
