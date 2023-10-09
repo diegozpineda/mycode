74a75781ffdee5ab633602414223c05676bd4fea
diegozpineda <dzpineda86@gmail.com>
2023-10-09 18:52:38 +0000

    Chapter 101 - Automating commands with ssh and paramiko

---
+++ paramiko_sshrsa/learntossh_old.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+''' Paramiko and ssh '''
+
+import os
+import paramiko
+
+def commandissue(command_to_issue, sshsession):
+    ssh_stdin, ssh_stdout, ssh_stderr = sshsession.exec_command(command_to_issue)
+    return ssh_stdout.read()
+
+def main():
+    sshsession = paramiko.SSHClient()
+
+    mykey = paramiko.RSAKey.from_private_key_file("/home/student/.ssh/id_rsa")
+
+    sshsession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    sshsession.connect(hostname="10.10.2.3", username="blender", pkey=mykey)
+
+    our_commands = ['touch sshworked.txt', 'touch create.txt', 'touch file3.txt', 'ls']
+
+    for cmd in our_commands:
+        print(commandissue(cmd, sshsession))
+
+main()
